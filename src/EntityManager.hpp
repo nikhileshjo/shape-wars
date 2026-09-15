9ce9890f34c88d5da17d7b32e7c4d50250922f30
Nikhilesh Joshi <nikhilesh.joshi.1999@gmail.com>
2026-09-15 23:38:44 +0530

    feat: add entity manager

---
+++ src/EntityManager.hpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include "Entity.hpp"
+#include <vector>
+#include <memory>
+#include <map>
+
+using EntityVec = std::vector<std::shared_ptr<Entity>>;
+using EntityMap = std::map<std::string, EntityVec>;
+
+class EntityManager
+{
+        EntityVec m_entities;
+        EntityMap m_entityMap;
+        EntityVec m_toAdd;
+        size_t    m_totalEntities = 0;
+    public:
+        EntityManager(){}
+
+        void                     update();                            // add/remove entites
+        std::shared_ptr<Entity>  addEntity(const std::string& tag);   // add entites to buffer 
+        EntityVec&               getEntities();                       // return m_entities
+        EntityVec&               getEntities(const std::string& tag); // return entities of a particular tag
+};
+
+
+// add to buffer
+std::shared_ptr<Entity> EntityManager::addEntity(const std::string& tag)
+{
+    auto e = std::shared_ptr<Entity>(new Entity(tag, m_totalEntities++));
+    m_toAdd.push_back(e);
+    return e;
+}
+
+void EntityManager::update()
+{
+    // adding entites if any in buffer
+    for (auto e : m_toAdd)
+    {
+        m_entities.push_back(e);
+        m_entityMap[e->getTag()].push_back(e);
+    }
+    m_toAdd.clear();
+    
+    // removing entities if dead
+    int i = 0;
+    int entityCnt = m_entities.size();
+    while (i < entityCnt)
+    {
+        if (!m_entities[i]->isAlive())
+        {
+            std::string eTag = m_entities[i]->getTag();
+            size_t eId = m_entities[i]->getId();
+            m_entities.erase(m_entities.begin() + i);
+            for (auto j = 0; j < m_entityMap[eTag].size(); j++)
+            {
+                if (m_entityMap[eTag][j]->getId() == eId)
+                {
+                    m_entityMap[eTag].erase(m_entityMap[eTag].begin() + j);
+                    break;
+                }
+            }
+
+            entityCnt--;
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
+
+EntityVec& EntityManager::getEntities()
+{
+    return m_entities;
+}
+
+EntityVec& EntityManager::getEntities(const std::string& tag)
+{
+    return m_entityMap[tag];
+}
